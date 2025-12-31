e822f6441c2e8411bc6e80d8c72c290762d8aa50
Himanshu singla <singlah2505@gmail.com>
2025-12-31 12:24:26 +0530

    initial commit

---
+++ TrustLink_backend/src/main/java/com/Singla/TrustLink_backend/TrustLinkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Singla.TrustLink_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustLinkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustLinkBackendApplication.class, args);
+	}
+
+}
---
+++ TrustLink_backend/src/test/java/com/Singla/TrustLink_backend/TrustLinkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Singla.TrustLink_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustLinkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
