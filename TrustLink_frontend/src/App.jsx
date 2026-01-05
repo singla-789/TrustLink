9a8a295757a12d5ae23e63c1b84446953fa599c4
Himanshu singla <singlah2505@gmail.com>
2026-01-06 02:24:58 +0530

    working

--- TrustLink_frontend/src/App.jsx
+++ TrustLink_frontend/src/App.jsx
@@ -2,14 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
+import LandingPage from './components/LandingPage'
+import AboutPage from './components/AboutPage'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='flex items-center justify-center h-full w-full'>
-      <div className='border font-light flex items-center'>hello</div> 
-    </div>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<LandingPage/>}/>
+      <Route path='/about' element= {<AboutPage/>}/>
+    </Routes>
+    <Footer/>
+    </BrowserRouter>
   )
 }
 
